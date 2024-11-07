afb308cfe9c01db2e7b022f173d7fe0f023d8a9c
guyong613 <ufogy@qq.com>
2024-11-07 14:57:33 +0800

    guy frist commit

---
+++ vtest.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://1107jjtax.net/
+// @version      2024-11-07
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.baidu.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log('Guy TEST GREASY!');
+    // Your code here...
+})();
